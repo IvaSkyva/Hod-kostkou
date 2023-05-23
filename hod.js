e8d0b32533520bfaffa4c3e319dc7c4ae6fd4a8f
Ivana Skýva <ivana.rybakova@gmail.com>
2023-05-23 23:49:03 +0200

    otaceni pri hodu

--- hod.js
+++ hod.js
@@ -1,10 +1,54 @@
+let score = document.querySelector('#score');
+score = 0;
+
+function increaseScore() {
+	score++ 
+   console.log("Skóre: " + score);
+   const scoreDisplay = document.getElementById("score");
+	 scoreDisplay.textContent = "Bodů: " + score;
+ }
+
 function hodKostkou() {
 	let hod = Math.floor(Math.random() * 6 + 1);
 
+	const kostka = document.getElementById("kostka");
+  const zprava = document.getElementById("zprava");
+
+
+
 	kostka.src = '/images/' + hod + '.png';
+
+	
 	if (hod === 6) {
-		zprava.textContent = 'Šestka!!! Vyhráli jste.'
+		
+	kostka.style.animation = 'none';
+	setTimeout(function() {
+	  kostka.style.animation = '';
+	}, 10);
+
+	setTimeout(function() {
+	zprava.style.display = 'block';
+	zprava.textContent = 'Šestka!!! Máte bod.';
+    increaseScore();
+    }, 4500);
+
+	
+	
 	} else {
-		zprava.textContent = 'Hodili jste ' + hod + '. Zkuste to znovu.';
-	}
+		
+	kostka.style.animation = 'none';
+	setTimeout(function() {
+	  kostka.style.animation = '';
+	}, 10);
+	
+	setTimeout(function() {
+	zprava.style.display = 'block';
+	zprava.textContent = 'Hodili jste ' + hod + '. Zkuste to znovu.';
+	}, 4500);
+
+	setTimeout(function() {
+		zprava.style.display = 'none';	
+		}, 6500);
+	
+     }
 }
